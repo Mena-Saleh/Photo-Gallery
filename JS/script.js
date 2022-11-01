5ec78a814676a7486e5c2f57ac07ee5b1945a01f
Mena_Ashraf <Mena.A.Saleh.2001@gmail.com>
2022-11-01 12:44:59 +0200

    LocalStorage Functionality

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-//Like/dislike heart animation + updating likes on the front end.
+//Like/dislike heart animation + updating likes on the front end only.
 function heartAnimation(event){
 
 const elem = event.target; //Get the element clicked on.
@@ -25,16 +25,18 @@ likesElem.innerHTML = likes;
 
 }
 
-//Delete clicked element
 
+//Delete clicked element
 function deleteElement(event)
 {
   
     const elem = event.target; //Get the element clicked on
     const isConfirmed = confirm("Are you sure you want to delete this photo?"); //Ask for confirmation:
-    if(isConfirmed) //Delete parent element (the whole card) if confirmed by user.
+    if(isConfirmed) //Delete parent element (the whole card) if confirmed by user and remove data from local storage.
     {
-        elem.parentElement.remove();
+        deleteCardFromLocalStorage(elem.getAttribute("data-id")); //Delete data from local storage.
+        localStorage.idCounter --; //Decrement element count.
+        elem.parentElement.remove(); //Remove HTML element without refreshing,
     }
 }
 
@@ -66,19 +68,20 @@ const getAPIData = async (baseURL , page, key) =>
 
 }
 
+
 //Function that adds a public photos card.
 
 const publicGrid = document.querySelector('#public-photos-grid');
 
 //Function that takes card information and creates a card element under public photos grid.
-function createCard (profilePictureURL, username, imageURL, imageName, imageDesc, Likes )
+function createPublicCard (profilePictureURL, username, imageURL, imageName, imageDesc, Likes )
 {
     //If the image name or description are null, replace the value "null" with empty quotes "":
     imageName = imageName == null? "": imageName;
     imageDesc = imageDesc == null? "": imageDesc;
 
     //Card element html:
-    const card = `<div class="card">
+    const publicCard = `<div class="card">
           
     <img class= "profile-pic" src="${profilePictureURL}" alt="profile picture">
     <div class="name">${username}<i class="fas fa-check"></i></div>
@@ -97,41 +100,90 @@ function createCard (profilePictureURL, username, imageURL, imageName, imageDesc
   </div>`
 
   //Append the card HTML to the grid's HTML:
-  publicGrid.innerHTML += card;
+  publicGrid.innerHTML += publicCard;
 
 }
 
 
 //Function that creates cards with API data
-function generateCardsFromData(data){
+function generateCardsFromAPIData(data){
     //Loop over all entries retreiving the required data and creating a card for each entry:
     for (let [key, value] of Object.entries(data)) {
         //For now I'll pass "Lorem Ipsum" for photo name, because the API doesn't provide photo names.
-        createCard(value.user.profile_image.small, value.user.username, value.urls.regular, "Lorem Ipsum" , value.description, value.likes)
+        createPublicCard(value.user.profile_image.small, value.user.username, value.urls.regular, "Lorem Ipsum" , value.description, value.likes)
     }
 }
 
 
 
-//Function Calls
 
+///Function that takes card information and creates a card element under local photos grid.
+
+const localGrid = document.querySelector('#local-photos-grid');
+
+function createLocalCard (card)
+{
+  const localCard = `<div class="card">
+          
+  <img class= "profile-pic" src="Local_Images/user.jpg" alt="profile picture">
+  <div class="name">thecolorlesseyes<i class="fas fa-check"></i></div>
+  <i class="fa-regular fa-trash-can trash-icon" onclick="deleteElement(event)" data-id="${card.id}"></i>
+  <div class="photo">
+    <img src="${card.img}" alt="user image">
+  </div>
+  <i class="fa-regular fa-heart heart-icon" onclick="heartAnimation(event)"></i>
+  <div class="photo-text">
+    <div class="photo-name-likes">
+      <h4>${card.name}</h4>
+      <h6><span class="like-count">0</span> Likes</h6>
+    </div>
+    <p>${card.desc}</p>
+  </div>
+  
+</div>`
+
+
+  //Append the card HTML to the grid's HTML:
+  localGrid.innerHTML += localCard;
+
+}
+
+//Function that creates cards from LocalStorage data
+
+function generateCardsFromLocalStorage(){
+  let cardKey;
+  for(let i = 0; i < localStorage.idCounter; i++) //Loop over all cards stored
+  {
+    let card = JSON.parse(localStorage["card"+i]);  //Get the ith card.
+    createLocalCard(card);
+  }
+}
+
+
+//Function Calls
 
 if(document.body.id == 'index-body') //Functions related to index page
 {
+
+//Generate local storage album:
+generateCardsFromLocalStorage();
 
 
 //To load the album every time the home page is refreshed or re-directed to.
 document.addEventListener("DOMContentLoaded", ()=> {
   //get a random pictures feed everytime and display 10 cards on the page:
   getAPIData(baseURL,Math.floor(Math.random() * 100), key).then(function(data){
-      generateCardsFromData(data);
+      generateCardsFromAPIData(data);
       });
 
 })
 
 
 
+
 } 
+
+
 else //Functions related to add-photo page
 {
 
@@ -145,12 +197,12 @@ const name = document.querySelector('#text-name');
 const desc = document.querySelector('#text-desc');
 const img = document.querySelector('#file-input');
 
+
+const regex = /^[a-zA-Z-,]*$/; //Regex for name validation (accepts alphabets . and -);
+
 button.addEventListener('click', (event) =>{
 
   event.preventDefault(); //Prevent sumbit action for now, to validate in JS first.
-
-
-  const regex = /^[a-zA-Z-,]*$/; //Regex for name validation (accepts alphabets . and -);
 
   
   if(form.checkValidity()) //Check if it passes initial HTML input validation.
@@ -166,6 +218,17 @@ button.addEventListener('click', (event) =>{
 
         if (imgType == 'image/jpg' || imgType == 'image/jpeg' || imgType == 'image/png') //Check if image is png, jpg or jpeg:
         {
+          const reader = new FileReader();
+          reader.readAsDataURL(img.files[0]);
+          
+          //Wait for the reader to read, then add a card to local storage with relevant info.
+            reader.addEventListener("load", ()=>{
+            const myCard = new Card(localStorage.idCounter== null? 0: localStorage.idCounter, name.value, desc.value, reader.result);
+            writeCardToLocalStorage(myCard);
+            
+          })
+          
+          alert("Success!");
           form.submit();
         }
         else{
@@ -190,9 +253,6 @@ button.addEventListener('click', (event) =>{
   }
 
 
-  // alert(name.value);
-  // alert(desc.value);
-  //alert(img.files[0].type);
 
 
 })
@@ -213,4 +273,34 @@ button.addEventListener('click', (event) =>{
 
 
 
+//Local storage manipulation and storing card data:
+
+//Class to represent card data:
+class Card  {
+  constructor(id,name, desc, img){
+    this.id = id;
+    this.name = name;
+    this.desc = desc;
+    this.img = img;
+  }
+}
+
+
+//Push a new card to the local storage:
+function writeCardToLocalStorage(card){
+  if(localStorage.idCounter == null) //Initialize counter if it doesn't exist
+  {
+    localStorage.idCounter = 0;
+  }
+    localStorage.setItem(`card${localStorage.idCounter}`, JSON.stringify(card)); //Set an item with unique ID storing the JSON object for the card as a string.
+    localStorage.idCounter ++; //Increment counter
+
+}
+
+
+//Deletes card from storage using id
+function deleteCardFromLocalStorage(id)
+{
+  localStorage.removeItem("card" + id);
+}
 
